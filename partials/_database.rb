687be04f0f6939abfabb0cce076b4ce6710f0f93
Sylvestre Mergulhão <contato@mergulhao.info>
2012-06-05 10:44:39 -0300

    add todo notice

--- partials/_database.rb
+++ partials/_database.rb
@@ -8,6 +8,9 @@ database_gem = case options[:database]
   else  "gem 'sqlite3'"
 end
 
+# TODO: SOMEONE REMOVED OUR DEFAULT DATABASE.YML CONFIG FILE.
+# IT SHOULD BE THE DEFAULT. ALSO POSTGRESQL SHOULD BE DEFAULT TOO.
+
 gsub_file 'Gemfile', /DATABASE_GEM/, database_gem
 
 git :add => '.'
