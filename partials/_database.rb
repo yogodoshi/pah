82b193bb8bc0c307d7162248f6fbfc662016f0cc
Pedro Nascimento <pnascimento@gmail.com>
2012-07-02 11:40:43 -0300

    Always defaults to postgreSQL.

--- partials/_database.rb
+++ partials/_database.rb
@@ -1,18 +1,7 @@
-puts "Add database(#{options[:database]}) gem support ...".magenta
-
 copy_static_file 'Gemfile'
-
-database_gem = case options[:database]
-  when "mysql"      then "gem 'mysql2',         '0.3.11'"
-  when "postgresql" then "gem 'pg',             '0.13.2'"
-  else  "gem 'sqlite3'"
-end
-
-# TODO: SOMEONE REMOVED OUR DEFAULT DATABASE.YML CONFIG FILE.
-# IT SHOULD BE THE DEFAULT. ALSO POSTGRESQL SHOULD BE DEFAULT TOO.
-
-gsub_file 'Gemfile', /DATABASE_GEM/, database_gem
+copy_static_file 'config/database.yml'
+gsub_file 'config/database.yml', /PROJECT/, @app_name
 
 git :add => '.'
-git :commit => "-aqm 'Add #{options[:database]} gem.'"
+git :commit => "-aqm 'Add postgres gem and Gemfile.'"
 puts "\n"
