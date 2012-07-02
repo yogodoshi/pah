04b35204d66404391915c23cfa637a3a333c7df7
Pedro Nascimento <pnascimento@gmail.com>
2012-07-02 17:20:54 -0300

    Extracting gems to a different partial. Adding rack-canonical-host to Gemfile and config.ru

--- partials/_database.rb
+++ partials/_database.rb
@@ -1,7 +1,6 @@
-copy_static_file 'Gemfile'
 copy_static_file 'config/database.yml'
 gsub_file 'config/database.yml', /PROJECT/, @app_name
+git :add => 'config/datase.yml'
+git :commit => "-m 'Adding config/database.yml.'"
 
-git :add => '.'
-git :commit => "-aqm 'Add postgres gem and Gemfile.'"
 puts "\n"
---
+++ partials/_gems.rb
@@ -0,0 +1,14 @@
+copy_static_file 'Gemfile'
+git :add => 'Gemfile'
+git :commit => "-qm 'Adding Gemfile.'"
+rack_canonical = <<CANONICAL
+
+#Run heroku config:add CANONICAL_HOST=yourdomain.com
+#For more information, see: https://github.com/tylerhunt/rack-canonical-host#usage
+use Rack::CanonicalHost, ENV['CANONICAL_HOST'] if ENV['CANONICAL_HOST']
+CANONICAL
+in_root do
+  inject_into_file 'config.ru', rack_canonical, {after: "require ::File.expand_path('../config/environment',  __FILE__)", verbose: false}
+end
+
+puts "\n"
