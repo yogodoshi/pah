220cd713429a0a03a312b8bd9e00e4966201d0a5
Pedro Nascimento <pnascimento@gmail.com>
2012-07-02 11:20:16 -0300

    Copy spec/spec_helper.rb to the project.

--- partials/_rspec.rb
+++ partials/_rspec.rb
@@ -3,6 +3,7 @@ puts "Setting up RSpec ... ".magenta
 remove_dir 'test'
 
 run "#{@rvm} exec rails generate rspec:install"
+copy_static_file 'spec/spec_helper.rb'
 
 git :add => '.'
 git :commit => "-aqm 'Configured RSpec.'"
