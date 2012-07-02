8a70d3f032ab526f6c8f17deee790cd55d971c7a
Pedro Nascimento <pnascimento@gmail.com>
2012-07-02 13:55:24 -0300

    Setting default generators.

---
+++ partials/_generators.rb
@@ -0,0 +1,13 @@
+generators = <<GENERATORS
+
+    #Do not generate specs for views and requests.
+    #Also, do not generate assets.
+    config.generators do |g|
+      g.view_specs false
+      g.assets false
+      g.integration_tool false
+    end
+GENERATORS
+in_root do
+  inject_into_file 'config/application.rb', generators, {after: "Rails::Application", verbose: false}
+end
