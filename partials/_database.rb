6cb8ad5baa91855f0886246375bc1053340541b0
Pedro Nascimento <pnascimento@gmail.com>
2012-07-02 17:57:48 -0300

    typos.

--- partials/_database.rb
+++ partials/_database.rb
@@ -1,6 +1,6 @@
 copy_static_file 'config/database.yml'
 gsub_file 'config/database.yml', /PROJECT/, @app_name
-git :add => 'config/datase.yml'
-git :commit => "-m 'Adding config/database.yml.'"
+git :add => 'config/database.yml'
+git :commit => "-qm 'Adding config/database.yml.'"
 
 puts "\n"
