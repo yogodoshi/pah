e1ff50507af703545d138d94a354ed18a09f88a0
Sylvestre Mergulhao <contato@mergulhao.info>
2012-07-31 14:40:39 -0300

    initialize_on_precompile to false by default

--- partials/_generators.rb
+++ partials/_generators.rb
@@ -1,13 +1,15 @@
 generators = <<GENERATORS
 
-    #Do not generate specs for views and requests.
-    #Also, do not generate assets.
+    # Do not generate specs for views and requests. Also, do not generate assets.
     config.generators do |g|
       g.helper false
       g.view_specs false
       g.assets false
       g.integration_tool false
     end
+
+    # Prevent initializing your application and connect to the database on assets precompile.
+    config.assets.initialize_on_precompile = false
 GENERATORS
 in_root do
   inject_into_file 'config/application.rb', generators, {after: "Rails::Application", verbose: false}
