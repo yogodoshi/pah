723fbab83d404d8acb2f9221f8e85de5be6f04fe
Rafael Lima <contato@rafael.adm.br>
2013-01-11 15:06:46 -0200

    add omniauth

--- partials/_omniauth.rb
+++ partials/_omniauth.rb
@@ -1,11 +1,13 @@
 puts "Add omniauth support ...".magenta
 
-%w{omniauth.rb}.each do |component|
-  copy_static_file "config/initializers/#{component}"
+if would_you_like? "Add omniauth support? [y,n]".red
+  %w{omniauth.rb}.each do |component|
+    copy_static_file "config/initializers/#{component}"
+  end
+
+  gsub_file 'Gemfile', /# gem \'omniauth/, "gem 'omniauth"
+
+  git :add => '.'
+  git :commit => "-aqm 'Add omniauth support.'"
+  puts "\n"
 end
-
-gsub_file 'Gemfile', /# gem 'omniauth/, "gem 'omniauth"
-
-git :add => '.'
-git :commit => "-aqm 'Add omniauth support.'"
-puts "\n"
