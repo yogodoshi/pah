7ec8649a63c85f74a009743c83cf909d114c2dfe
Matheus Bras <matheus.bras@hotmail.co.uk>
2013-05-13 15:41:15 -0300

    Added letter_opener gem for opening emails on development

--- partials/_generators.rb
+++ partials/_generators.rb
@@ -16,8 +16,22 @@ generators = <<GENERATORS
     # Prevent initializing your application and connect to the database on assets precompile.
     config.assets.initialize_on_precompile = false
 GENERATORS
+
 in_root do
   inject_into_file 'config/application.rb', generators, {after: "Rails::Application", verbose: false}
 end
+
 git :add => 'config/application.rb'
 git :commit => "-qm 'Adding generators.'"
+
+letter_opener_config = <<LETTER_OPENER
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+LETTER_OPENER
+
+in_root do
+  inject_into_file 'config/environments/development.rb', letter_opener_config, { before: "end", verbose: false }
+end
+
+git :add => 'config/environments/development.rb'
+git :commit => "-qm 'Adding letter_opener config.'"
