d1137f86ca518fd5593c12fe9e5acba1db3c3003
Rodrigo Pinto <rodrigopqn@gmail.com>
2013-06-13 21:43:07 -0300

    Adding heroku-deploy gem and making changes to make it fit on the project.

--- partials/_default.rb
+++ partials/_default.rb
@@ -4,20 +4,12 @@ copy_static_file 'app/assets/stylesheets/reset.css'
 copy_static_file 'app/views/layouts/application.html.haml'
 copy_static_file 'config/unicorn.rb'
 copy_static_file 'Procfile'
-
-
-%w{requires.rb}.each do |component|
-  copy_static_file "config/initializers/#{component}"
-end
-
-%w{integration.rake deploy.rake}.each do |component|
-  copy_static_file "lib/tasks/#{component}"
-end
+copy_static_file "config/initializers/requires.rb"
+copy_static_file "lib/tasks/integration.rake"
 
 copy_static_file 'config/locales/pt-BR.yml'
 copy_static_file 'public/index.html' if ENV['RAILS_TEMPLATE_TEST'] == 'true'
 
-gsub_file 'lib/tasks/deploy.rake', /PROJECT/, @app_name
 gsub_file 'lib/tasks/integration.rake', /PROJECT/, @app_name
 
 copy_static_file '.gitignore'
