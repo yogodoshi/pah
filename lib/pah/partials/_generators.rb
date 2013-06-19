3edd2408253429a41ed6b129ea1e529ec4e13a1f
Mauro George <maurogot@gmail.com>
2013-06-19 10:27:40 -0300

    fixed letter_opener bug

--- lib/pah/partials/_generators.rb
+++ lib/pah/partials/_generators.rb
@@ -25,13 +25,16 @@ git :add => 'config/application.rb'
 git :commit => "-qm 'Adding generators.'"
 
 letter_opener_config = <<LETTER_OPENER
+
+  # Letter openner config
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 LETTER_OPENER
 
 in_root do
-  inject_into_file 'config/environments/development.rb', letter_opener_config, { before: "end", verbose: false }
+  inject_into_file 'config/environments/development.rb', letter_opener_config, { before: /^end/, verbose: false }
 end
 
 git :add => 'config/environments/development.rb'
 git :commit => "-qm 'Adding letter_opener config.'"
+
