feb7e98ef96e3663352df65627a6cc9237d5dddc
Pedro Nascimento <pnascimento@gmail.com>
2013-06-29 15:37:27 -0300

    Fixing with: :exception weird text on ApplicationController.

--- lib/pah/partials/_secure_headers.rb
+++ lib/pah/partials/_secure_headers.rb
@@ -4,7 +4,7 @@ content = <<EOF
   ensure_security_headers # See more: https://github.com/twitter/secureheaders
 EOF
 in_root do
-  inject_into_file 'app/controllers/application_controller.rb', content, {after: "protect_from_forgery", verbose: false}
+  inject_into_file 'app/controllers/application_controller.rb', content, {after: "exception", verbose: false}
 end
 git :add => 'app/controllers/application_controller.rb'
 git :commit => "-qm 'Adding secure headers.'"
