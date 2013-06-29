7790ba6e5e9e1a00b6c790c9a7f0fed59d69fd1b
Pedro Nascimento <pnascimento@gmail.com>
2013-06-29 17:57:16 -0300

    Update _secure_headers.rb

--- lib/pah/partials/_secure_headers.rb
+++ lib/pah/partials/_secure_headers.rb
@@ -4,7 +4,7 @@ content = <<EOF
   ensure_security_headers # See more: https://github.com/twitter/secureheaders
 EOF
 in_root do
-  inject_into_file 'app/controllers/application_controller.rb', content, {after: "exception", verbose: false}
+  inject_into_file 'app/controllers/application_controller.rb', content, {after: "with: :exception", verbose: false}
 end
 git :add => 'app/controllers/application_controller.rb'
 git :commit => "-qm 'Adding secure headers.'"
