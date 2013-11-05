1a8912b3811b95c00ece5d6fe936c7b7e4e04457
Sylvestre Mergulhão <contato@mergulhao.info>
2013-11-05 10:47:15 -0200

    set current ruby and gemset to .ruby-version and .ruby-gemset.

--- lib/pah/partials/_rvm.rb
+++ lib/pah/partials/_rvm.rb
@@ -1,35 +1,16 @@
 # Set up rvm private gemset
 require 'rvm'
-puts "Setting up RVM gemset ... ".magenta
+puts "Setting up ruby env ... ".magenta
 
-# Need to strip colors in case rvm_pretty_print_flag is enabled in user's .rvmrc
-rvm_list = `rvm list`.gsub(Regexp.new("\e\\[.?.?.?m"), '')
-
-current_ruby = rvm_list.match(/=.? ([^ ]+)/)[1]
-desired_ruby = current_ruby.gsub(/\-p\d+/, "")
-
-@env = RVM::Environment.new(desired_ruby)
-
-gemset_name = @app_name
-
-puts "Creating gemset #{gemset_name} in #{desired_ruby}"
-@env.gemset_create(gemset_name)
-puts "Now using gemset #{gemset_name}"
-@env.gemset_use!(gemset_name)
-
-rvm_current = `rvm current`.match(/[\w\d.-]+@[\w\d\.-]+/)[0].strip.split('@').last
-
-if rvm_current != gemset_name
-  puts "Error using gemset #{gemset_name}:".red
-  puts "#{rvm_current} does not match the gemset #{gemset_name}".yellow
-  exit
-end
+current_ruby_env = `rvm-prompt i v g`.split("@")
+current_ruby = current_ruby_env[0]
+current_gemset = current_ruby_env[1]
 
 copy_static_file '.ruby-version'
-gsub_file '.ruby-version', /RUBY_VERSION/, desired_ruby
+gsub_file '.ruby-version', /RUBY_VERSION/, current_ruby
 
 copy_static_file '.ruby-gemset'
-gsub_file '.ruby-gemset', /GEMSET/, gemset_name
+gsub_file '.ruby-gemset', /GEMSET/, current_gemset
 
 git :add => '.ruby-version'
 git :add => '.ruby-gemset'
