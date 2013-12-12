96fe2eb4f1319553257eeb1dc60e45f1352b23c2
Ali Ismayilov <ali@ismailov.info>
2013-12-12 17:14:11 -0200

    reset.scss is copied in _default partial

--- lib/pah/partials/_stylesheets.rb
+++ lib/pah/partials/_stylesheets.rb
@@ -2,7 +2,6 @@ puts "Adding stylesheets files ...".magenta
 
 copy_static_file 'app/assets/stylesheets/application.css.scss'
 remove_file 'app/assets/stylesheets/application.css'
-copy_static_file 'app/assets/stylesheets/reset.css'
 
 git :add => '--all'
 git :commit => "-aqm 'Add stylesheets files.'"
