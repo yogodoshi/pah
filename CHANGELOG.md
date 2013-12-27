60c7e970073a17fcfa606b7f5049bce88adc73b2
Eduardo Fiorezi <eduardofiorezi@gmail.com>
2013-12-27 11:28:49 -0200

    Preserves favicon.ico and robots.txt

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,6 +21,7 @@
 - Update project to Rails 4.0.2
 - Update project gem versions
 - Move rails_12factor to a group block
+- Not remove favicon.ico and robots.txt from pah cleanup task
 
 ### bug fixes
 
--- lib/pah/partials/_cleanup.rb
+++ lib/pah/partials/_cleanup.rb
@@ -3,11 +3,6 @@ puts "Removing unnecessary files ... ".magenta
 remove_file "README.rdoc"
 remove_file "app/views/layouts/application.html.erb"
 
-inside "public" do
-	remove_file "favicon.ico"
-	remove_file "robots.txt"
-end
-
 git :add => '--all'
 git :commit => "-aqm 'Removed unnecessary files left over from initial app generation.'"
 puts "\n"
