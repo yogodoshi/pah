c2a20ab7fe35913ea17e33cf3f2f08dc1d7cc26c
Thiago Belem <contato@thiagobelem.net>
2013-12-27 11:12:04 -0200

    Replace integration.rake with jumup-heroku

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -23,6 +23,7 @@
 - Move rails_12factor to a group block
 - Not remove favicon.ico from generated app
 - Set ruby 2.1.0 as default
+- Use jumpup-heroku gem on integration
 
 ### bug fixes
 
--- lib/pah/partials/_integration.rb
+++ lib/pah/partials/_integration.rb
@@ -1,9 +1,12 @@
 puts "Setting up Integration... ".magenta
 
 copy_static_file 'lib/tasks/integration.rake'
-gsub_file 'lib/tasks/integration.rake', /PROJECT/, (@config[:heroku][:name] || @app_name)
+copy_static_file 'config/initializers/jumpup_heroku.rb'
+
+gsub_file 'config/initializers/jumpup_heroku.rb', /PROJECT/, (@config[:heroku][:name] || @app_name)
 
 git :add => 'lib/tasks/integration.rake'
-git :commit => "-qm 'Adding integration tasks.'"
+git :add => 'config/initializers/jumpup_heroku.rb'
+git :commit => "-qm 'Adding integration tasks and configuration.'"
 
 puts "\n"
