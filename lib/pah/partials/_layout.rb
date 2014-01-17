109a07d0aff10495b7956333f4e8a97dd8037cc1
Eduardo Fiorezi <eduardofiorezi@gmail.com>
2014-01-17 17:37:24 -0200

    Add app_name to page_title. This closes #162

--- lib/pah/partials/_layout.rb
+++ lib/pah/partials/_layout.rb
@@ -4,6 +4,8 @@ copy_static_file 'app/views/layouts/application.html.haml'
 copy_static_file 'app/views/application/_error_messages.html.haml'
 copy_static_file 'app/views/application/_flash_messages.html.haml'
 
+gsub_file 'app/views/layouts/application.html.haml', /page_title/, "page_title(app_name: '#{@app_name}')"
+
 git :add => '.'
 git :commit => "-aqm 'Add layout files.'"
 puts "\n"
