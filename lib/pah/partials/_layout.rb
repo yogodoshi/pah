ceeaf373305ad1b6427f24095b36241d08c60cdb
Ali Ismayilov <ali@ismailov.info>
2014-02-07 10:45:17 -0200

    replace page_title method call fully. fixes #183

--- lib/pah/partials/_layout.rb
+++ lib/pah/partials/_layout.rb
@@ -4,7 +4,7 @@ copy_static_file 'app/views/layouts/application.html.haml'
 copy_static_file 'app/views/application/_error_messages.html.haml'
 copy_static_file 'app/views/application/_flash_messages.html.haml'
 
-gsub_file 'app/views/layouts/application.html.haml', /= page_title/, "= page_title(app_name: '#{@app_name}')"
+gsub_file 'app/views/layouts/application.html.haml', /= page_title(app_name: 'example')/, "= page_title(app_name: '#{@app_name}')"
 
 git add: '.'
 git commit: "-aqm 'Add layout files.'"
