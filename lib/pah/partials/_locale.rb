b3dc92eb642f42e95443c0c613cc45465052daba
Ali Ismayilov <ali@ismailov.info>
2014-02-10 14:17:35 -0200

    refactor git adding locale files

--- lib/pah/partials/_locale.rb
+++ lib/pah/partials/_locale.rb
@@ -1,17 +1,11 @@
 puts "Adding locale... ".magenta
 
 copy_static_file 'config/locales/pt-BR.yml'
-git add: 'config/locales/pt-BR.yml'
-
 copy_static_file 'config/locales/devise.pt-BR.yml'
-git add: 'config/locales/devise.pt-BR.yml'
-
 copy_static_file 'config/locales/admin.pt-BR.yml'
-git add: 'config/locales/admin.pt-BR.yml'
-
 copy_static_file 'config/locales/app.pt-BR.yml'
-git add: 'config/locales/app.pt-BR.yml'
 
+git add: 'config/locales/*.yml'
 git commit: "-qm 'Add pt-BR locale files.'"
 
 time_zone_config = <<TIME_ZONE
