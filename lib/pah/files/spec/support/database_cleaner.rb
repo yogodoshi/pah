8babab6bf49caae361674ee34df7f4a6e90cffbe
Ali Ismayilov <ali@ismailov.info>
2014-02-14 14:34:31 -0200

    add database_cleaner rule for js specs

--- lib/pah/files/spec/support/database_cleaner.rb
+++ lib/pah/files/spec/support/database_cleaner.rb
@@ -4,6 +4,10 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :deletion
+  end
+
   config.before(:each) do
     DatabaseCleaner.start
   end
