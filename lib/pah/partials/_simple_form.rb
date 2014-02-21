3471c32353445c255c1fb2a7f8fd78c5a163c07a
Fabio Rehm <fgrehm@gmail.com>
2014-02-21 14:04:06 -0300

    Add simple_form pt-BR locale

--- lib/pah/partials/_simple_form.rb
+++ lib/pah/partials/_simple_form.rb
@@ -5,3 +5,7 @@ end
 git add: 'config'
 git add: 'lib/templates'
 git_commit 'Run `rails generate simple_form:install`'
+
+copy_static_file 'config/locales/simple_form.pt-BR.yml'
+git add: 'config'
+git_commit 'Add simple_form pt-BR locale.'
