f2904aea2352fceb123c7cbb24ec665dbe9b8491
Tomás Augusto Müller <tomas.am@gmail.com>
2014-04-04 11:13:33 -0300

    Add config/puma.rb to Pah _puma.rb partial file.

--- lib/pah/partials/_puma.rb
+++ lib/pah/partials/_puma.rb
@@ -1,4 +1,6 @@
 copy_static_file 'config/initializers/database_connection.rb'
+copy_static_file 'config/puma.rb'
 
 git add: 'config/initializers/database_connection.rb'
+git add: 'config/puma.rb'
 git_commit 'Configure Puma.'
