6d623f90ace9257915d9ed4a11c265f1afa0726c
Tomás Augusto Müller <tomas.am@gmail.com>
2014-04-11 15:53:26 -0300

    Add config/initializers/puma_auto_tune.rb file to _puma.rb partial.

--- lib/pah/partials/_puma.rb
+++ lib/pah/partials/_puma.rb
@@ -1,4 +1,6 @@
 copy_static_file 'config/initializers/database_connection.rb'
+copy_static_file 'config/initializers/puma_auto_tune.rb'
 
 git add: 'config/initializers/database_connection.rb'
+git add: 'config/initializers/puma_auto_tune.rb'
 git_commit 'Configure Puma.'
