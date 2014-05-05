dad0873ae8d66d0ebbbea1fb5731e190730ec291
Tomás Augusto Müller <tomas.am@gmail.com>
2014-05-05 10:20:32 -0300

    Revert "Add  on , to configure rack-timeout into application middleware."
    This reverts commit f0c1e468527a3091d85c299de482918738dfa7b9.

--- lib/pah/partials/_rack_timeout.rb
+++ lib/pah/partials/_rack_timeout.rb
@@ -1,15 +1,4 @@
-rack_timeout = <<RACK_TIMEOUT
-
-use Rack::Timeout
-RACK_TIMEOUT
-
-in_root do
-  inject_into_file 'config.ru', rack_timeout, {after: "require ::File.expand_path('../config/environment',  __FILE__)", verbose: false}
-end
-
-git add: 'config.ru'
-
 copy_static_file 'config/initializers/rack_timeout.rb'
-git add: 'config/initializers/rack_timeout.rb'
 
+git add: 'config/initializers/rack_timeout.rb'
 git_commit 'Add Rack::Timeout configuration.'
