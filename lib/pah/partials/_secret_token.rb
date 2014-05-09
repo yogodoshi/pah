32d605bb37d8b06a7ddf95316ea54ecb0442d0d9
Eduardo Fiorezi <eduardofiorezi@gmail.com>
2014-05-09 18:12:36 -0300

    Fix secret key generation and use new rails name pattern

--- lib/pah/partials/_secret_token.rb
+++ lib/pah/partials/_secret_token.rb
@@ -1,10 +1,9 @@
 in_root do
-  append_to_file '.env', "SECRET_TOKEN: #{SecureRandom::hex(60)}\n"
+  append_to_file '.env', "SECRET_KEY_BASE: #{SecureRandom::hex(60)}\n"
 
-  gsub_file 'config/initializers/secret_token.rb', /config.secret_token = '(.*?)'/, "config.secret_token = ENV['SECRET_TOKEN']"
-  gsub_file 'config/initializers/secret_token.rb', /config.secret_key_base = '(.*?)'/, "config.secret_key_base = ENV['SECRET_TOKEN']"
+  gsub_file 'config/secrets.yml', /secret_key_base: (.*)/, "secret_key_base: <%= ENV[\"SECRET_KEY_BASE\"] %>"
 end
 
-git add: 'config/initializers/secret_token.rb'
+git add: 'config/secrets.yml'
 git add: '.env'
-git_commit 'Replace secret token with environment variable.'
+git_commit 'Replace secret key base with environment variable.'
