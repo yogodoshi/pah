4a6c2b4355e20819b4deae15928befb37c8de0f1
Fabio Rehm <fgrehm@gmail.com>
2014-05-23 19:04:59 -0300

    Install bundler only when needed in order to speed up the build

--- lib/pah/partials/_gems.rb
+++ lib/pah/partials/_gems.rb
@@ -3,13 +3,17 @@ copy_static_file 'Gemfile'
 in_root do
   gsub_file 'Gemfile', /RAILS_VERSION/, ::Pah::RAILS_VERSION
 
-  # Since the gemset is likely empty, manually install bundler so it can install the rest
-  if !(run "gem install bundler --no-ri --no-rdoc")
-    puts "Error installing bundler, will attempt to continue"
+  begin
+    require 'bundler'
+  rescue LoadError
+    # Install bundler if needed
+    if !(run "gem install bundler --no-ri --no-rdoc")
+      puts "Error installing bundler, will attempt to continue"
+    end
+    require 'bundler'
   end
 
   # Install all other gems needed from Gemfile
-  require 'bundler'
   Bundler.with_clean_env do
     if !(run "bundle install --jobs=4")
       puts "Error installing gems, aborting"
