88ad1d61b4255291aa221d19836d4cfd0c921be4
Mauro George <maurogot@gmail.com>
2014-05-23 17:50:22 -0300

    Change to run to exec

--- lib/pah/partials/_rollbar.rb
+++ lib/pah/partials/_rollbar.rb
@@ -1,4 +1,4 @@
-run "bundle exec rails generate rollbar"
+exec "bundle exec rails generate rollbar"
 
 gsub_file 'config/initializers/rollbar.rb', /Rails\.env\.test\?/, "Rails.env.production?"
 gsub_file 'config/initializers/rollbar.rb', /config\.enabled = false/, "config.enabled = true"
