198f2b3e3f30f05c6809180f88c4681bda114e6f
Thiago Belem <contato@thiagobelem.net>
2014-05-30 15:00:59 -0300

    Increase Aruba timeout to 5 minutes

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,5 +5,5 @@ root = Pathname.new(__FILE__).parent.parent.parent
 ENV['PATH'] = "#{root}/features/support/bin#{File::PATH_SEPARATOR}#{ENV['PATH']}"
 
 Before do
-  @aruba_timeout_seconds = 60 * 3
+  @aruba_timeout_seconds = 60 * 5
 end
