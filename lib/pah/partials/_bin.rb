12eb4fc95c237f2ffbe5a9af1f1e9891ef6c69e6
Mauro George <maurogot@gmail.com>
2014-05-30 17:23:53 -0300

    Remove unused Bundler and in_root

--- lib/pah/partials/_bin.rb
+++ lib/pah/partials/_bin.rb
@@ -1,8 +1,4 @@
-in_root do
-  Bundler.with_clean_env do
-    system 'bundle exec spring binstub --all'
-  end
-end
+system 'bundle exec spring binstub --all'
 
 git add: 'bin/rake bin/rails bin/spring'
 git_commit 'Add spring bin files'
