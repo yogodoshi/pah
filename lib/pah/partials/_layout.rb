18009cfe62cec104bdadc1335f15699c6cbf3ab8
Mauro George <maurogot@gmail.com>
2014-06-27 16:13:11 -0300

    Remove generated Assets
    * Remove all generated CSS by Pah
    * Remove all generated JS by Pah
    * Remove _error_messages.html.slim partial since we are using simple_form
    * Remove extra markup from layout application

--- lib/pah/partials/_layout.rb
+++ lib/pah/partials/_layout.rb
@@ -1,5 +1,4 @@
 copy_static_file 'app/views/layouts/application.html.slim'
-copy_static_file 'app/views/application/_error_messages.html.slim'
 copy_static_file 'app/views/application/_flash_messages.html.slim'
 
 gsub_file 'app/views/layouts/application.html.slim', /= page_title(app_name: 'example')/, "= page_title(app_name: '#{@app_name}')"
