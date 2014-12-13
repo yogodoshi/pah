a8119c2c1a8bb4892c1c33749b7a5740c819b0bf
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-13 17:30:12 -0200

    Simplify extract_global_options!

--- lib/pah/cli.rb
+++ lib/pah/cli.rb
@@ -6,25 +6,15 @@ module Pah
 
       def start(argv)
         command = Commands.find_by_name(argv[0])
-        command.new(extract_global_options(argv)).run(argv)
+        command.new(extract_global_options!(argv)).run(argv)
       end
 
       private
-        def extract_global_options(argv)
-          options = default_options
-          argv.delete_if do |arg|
-            if arg == "--verbose"
-              options[:verbose] = true
-              next true
-            end
-          end
+        def extract_global_options!(argv)
+          options = {}
+          options[:verbose] = !!argv.delete("--verbose")
           options
         end
-
-        def default_options
-          { verbose: false }
-        end
-
     end
   end
 end
