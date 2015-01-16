d4c0e4389367ee66f2b1e86bde2a698306cc7246
Mauro George <maurogot@gmail.com>
2015-01-16 15:20:54 -0200

    Fix identation of conanical host

--- lib/pah/templates/canonical_host.rb
+++ lib/pah/templates/canonical_host.rb
@@ -4,9 +4,9 @@ module Pah
       def call
         rack_canonical = <<CANONICAL
 
-      #Run heroku config:add CANONICAL_HOST=yourdomain.com
-      #For more information, see: https://github.com/tylerhunt/rack-canonical-host#usage
-      use Rack::CanonicalHost, ENV['CANONICAL_HOST'] if ENV['CANONICAL_HOST']
+#Run heroku config:add CANONICAL_HOST=yourdomain.com
+#For more information, see: https://github.com/tylerhunt/rack-canonical-host#usage
+use Rack::CanonicalHost, ENV['CANONICAL_HOST'] if ENV['CANONICAL_HOST']
 CANONICAL
 
         inject_into_file 'config.ru', rack_canonical, after: "require ::File.expand_path('../config/environment', __FILE__)", verbose: false
